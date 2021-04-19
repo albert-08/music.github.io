9c3be0ba60ec74c0da2f48a024f35a111e27e1b8
alberto <alberto.floreso94@gmail.com>
2021-04-19 16:27:15 -0500

    add vuex

--- src/components/TrackDetail.vue
+++ src/components/TrackDetail.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="container">
+    <div class="container" v-if="track && track.id">
         <div class="columns">
             <div class="column is-3 has-text-centered">
                 <figure class="media-left">
                     <p class="image">
                         <img :src="track.album.images[0].url" alt="">
                     </p>
-                    <p>
+                    <p class="button-bar">
                         <a class="button is-primary is-large">
                             <span class="icon" @click="selectTrack">
-
+                                ▶️
                             </span>
                         </a>
                     </p>
@@ -18,7 +18,7 @@
             <div class="column is-8">
                 <div class="panel">
                     <div class="panel-heading">
-                        <h1>{{ track.name }}</h1>
+                        <h1>{{ trackTitle }}</h1>
                     </div>
                     <div class="panel-block">
                         <article class="media">
@@ -52,24 +52,29 @@
 </template>
 
 <script>
+import { mapState, mapActions, mapGetters } from 'vuex'
+
 import trackMixin from '@/mixins/track'
-import trackService from '@/services/track'
 
 export default {
     mixins: [ trackMixin ],
     
-    data () {
-        return {
-            track: {}
-        }
+    computed: {
+        ...mapState(['track']),
+        ...mapGetters(['trackTitle'])
     },
     created () {
         const id = this.$route.params.id
 
-        trackService.getById(id)
-            .then(res => {
-                this.track = res
-            })
+        if (!this.track || !this.track.id || this.track.id !== id) {
+            this.getTrackById({ id })
+                .then(() => {
+                    console.log('Track loaded...')
+                })
+        }
+    },
+    methods: {
+        ...mapActions(['getTrackById'])
     }
 }
 </script>
@@ -77,5 +82,9 @@ export default {
 <style lang="scss" scoped>
     .columns {
         margin: 20px;
+    }
+
+    .button-bar {
+        margin-top: 20px;
     }
 </style>
